90ecd5f3672340764df6fa3cd0a5425daa6277d7
sergonzalezgomez <43619335+sergonzalezgomez@users.noreply.github.com>
2018-10-03 13:43:37 +0200

    Muestra en pantalla la Localidad de residencia actual

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba{
  public static void main(String ags[]){
   System.out.println("Sergio González Gómez");
   System.out.println("león");
+  System.out.println("Carbajal de la legua");
  }
 }
